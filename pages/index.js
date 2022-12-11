c6bc692b98ee6be23f76e4ea8e69c18f5cf96723
M M Khairul Haque <mkhaque29@gmail.com>
2022-12-11 18:36:27 +0100

    chnages on features

--- pages/index.js
+++ pages/index.js
@@ -171,9 +171,9 @@ export default function Home() {
             </div>
             <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
             <div class="absolute inset-0 flex opacity-0 translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0">
-            <h1 class="font-dmserif text-3xl font-bold text-white">Product Card</h1>
-            <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Hover style design for products card.</p>
-            <a href='https://mkhaque1.github.io/product-card-hover/' target="_blank" class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">Live</a>
+            <h1 class="font-dmserif text-3xl font-bold text-white">Weather App</h1>
+            <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Weather app design with html css vanila javascripts.</p>
+            <a href='https://mkhaque1.github.io/weather-app-2.0/' target="_blank" class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">Live</a>
             </div>
             </div>
           </div>
